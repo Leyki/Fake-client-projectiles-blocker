71a8a5819adb6569ae4a06cf624a1f9c4843e6b2
Leyki <ocsomrak@gmail.com>
2018-02-08 17:51:47 -0300

    Fix if it wasen't working before, sorry > .<

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ module.exports = function FakeClientProjectilesBlocker(dispatch) {
     let gameId,
         projectileId
 
-    dispatch.hook('S_LOGIN', 9, event => { gameId = event.gameId })
+    dispatch.hook('S_LOGIN', 9, event => { gameId = event.gameId; projectileId = event.gameId; })
 
     dispatch.hook('S_START_USER_PROJECTILE', 4, event => {
         if (gameId.equals(event.gameId)) projectileId = event.id
-        else (projectileId = gameId)
     })
 
     dispatch.hook('C_HIT_USER_PROJECTILE', 3, event => {
