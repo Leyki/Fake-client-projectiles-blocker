40dbc423957b7ad038a2d4f91b5bca9a5b5e075d
Leyki <ocsomrak@gmail.com>
2018-09-13 19:59:34 -0300

    update for 74, add usage for tera-game-state where possible, refactor a bit(?)

--- README.md
+++ README.md
@@ -2,12 +2,4 @@
 
 Rewritten by [Kasea](https://github.com/Kaseaa) to work with multiple projectiles skills since it wasn't working before, please redownload if you have an old release!
 
-This module will basically stop the client from sending unused client projectiles packets sent in reaction to others players projectiles being received to avoid consuming completely unnecesary net bandwidth.
-
-#### Requirements
-
-You must have `S_START_USER_PROJECTILE`, `C_HIT_USER_PROJECTILE` and `S_END_USER_PROJECTILE` mapped with their respective last defs.
-
-#### Usage
-
-Just leave it installed, no need for doing anything.
+This module will basically stop the client from sending invalid projectile packets sent in reaction to other players projectiles being received to avoid consuming completely unnecesary net bandwidth.
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-module.exports = function FakeClientProjectilesBlocker(dispatch) {
-    let gameId;
+module.exports = function FakeClientProjectilesBlocker(mod) {
+    mod.game.initialize("me");
     let projectiles = {};
 
-    dispatch.hook('S_LOGIN', 10, e=> { gameId = e.gameId; });
-
-    dispatch.hook('S_START_USER_PROJECTILE', 5, e=> {
-        projectiles[e.id.toString()] = gameId.equals(e.gameId);
-    });
-
-    dispatch.hook('C_HIT_USER_PROJECTILE', 4, e=> projectiles[e.id.toString()]);
-
-    dispatch.hook('S_END_USER_PROJECTILE', 3, e=> {
-        delete projectiles[e.id.toString()];
-    });
+    mod.hook('S_START_USER_PROJECTILE', dispatch.base.MajorPatchVersion >= 75 ? 9 : 8, e => { projectiles[e.id.toString()] = mod.game.me.is(e.gameId) });
+    mod.hook('C_HIT_USER_PROJECTILE', 4, e => projectiles[e.id.toString()]);
+    mod.hook('S_END_USER_PROJECTILE', dispatch.base.MajorPatchVersion >= 75 ? 4 : 3, e => { delete projectiles[e.id.toString()]; });
 }
