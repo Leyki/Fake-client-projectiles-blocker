451debeb9d500947bc620f04f48423dba98cc7b0
Leyki <ocsomrak@gmail.com>
2018-09-13 20:08:41 -0300

    copy pasterino without good iqerino no gooderino

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function FakeClientProjectilesBlocker(mod) {
     mod.game.initialize("me");
     let projectiles = {};
 
-    mod.hook('S_START_USER_PROJECTILE', dispatch.base.MajorPatchVersion >= 75 ? 9 : 8, e => { projectiles[e.id.toString()] = mod.game.me.is(e.gameId) });
+    mod.hook('S_START_USER_PROJECTILE', mod.base.MajorPatchVersion >= 75 ? 9 : 8, e => { projectiles[e.id.toString()] = mod.game.me.is(e.gameId) });
     mod.hook('C_HIT_USER_PROJECTILE', 4, e => projectiles[e.id.toString()]);
-    mod.hook('S_END_USER_PROJECTILE', dispatch.base.MajorPatchVersion >= 75 ? 4 : 3, e => { delete projectiles[e.id.toString()]; });
+    mod.hook('S_END_USER_PROJECTILE', mod.base.MajorPatchVersion >= 75 ? 4 : 3, e => { delete projectiles[e.id.toString()]; });
 }
